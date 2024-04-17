93a9a4eb494e09909ae50d52f068da49795791fb
kudzaishe muzata <kudzaishemmuzata@gmail.com>
2024-04-17 11:51:29 +0300

    tlm projrct

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  taskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const taskInput = document.getElementById('new-task-description');
+    const taskDes = taskInput.value;
+    const list = document.createElement('li');
+    list.textContent = taskDes;
+    taskList.appendChild(list);
+    taskInput.value = '';
+    taskList.addEventListener('click', () => {
+      list.remove();
+    });
+  })
 });
